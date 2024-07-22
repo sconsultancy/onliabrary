af974bc0238cc42e9136ab0c2abfc1cc23ecf921
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-07-22 23:58:46 +0530

    3rd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,13 @@ function Navbar() {
   return (
     <div className="flex justify-around mt-6">
       <div className=" flex">
-        {/* <img src="/logo.png" alt="" />{" "} */}
+        <img src="/logo.png" alt="" />{" "}
         <h1 className=" logo_text text-[28px] ml-2 text-[#BA131C] ">
           ONLIABRARY
         </h1>
       </div>
       <div className="flex space-x-8">
-        {/* <a className=" text-[15px] text-[#BA131C]" href="/features">
+        <a className=" text-[15px] text-[#BA131C]" href="/features">
           Features
         </a>
         <a className=" text-[15px] text-[#BA131C]" href="/pricing">
@@ -23,7 +23,7 @@ function Navbar() {
         </a>
         <a className=" text-[15px] text-[#BA131C]" href="/ebooks">
           E-Books
-        </a> */}
+        </a>
       </div>
       <div>
         <button className=" border  py-2 px-8 rounded border-[#BA131C] text-[#BA131C]">
